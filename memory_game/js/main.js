08d356da95e1339d652e085f9a703b0e7cf5e99b
Nhung Tran <nhungtran@Nhungs-MacBook-Pro.local>
2019-01-17 11:03:23 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
-console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push("queen");
+
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+
+cardsInPlay.push("king");
+
+console.log("User flipped king");
+
+if (length === 2){
+	// How many items are in the cardsInPlay array.
+} else if (cardsInPlay[0] === cardsInPlay [2]) {
+	console.log("You found a match!")
+	// Checks equality between the two cards in the cardsInPlay array. 
+} else {
+	console.log("Sorry, try again.")
+	// Values are not equal.
+}
