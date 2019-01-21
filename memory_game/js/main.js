1df095272aebd369174f82db0b5df3ef1cd2600e
Nhung Tran <nhungtrannn@gmail.com>
2019-01-21 14:47:28 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,30 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = "queen";
-var cardTwo = "king";
+var cardOne = cards[0];
+var cardTwo = cards[1];
 var cardThree = "";
 var cardFour = "";
 
-cardsInPlay.push("queen"); {
+cardsInPlay.push(cardOne); {
 
 	console.log("User played queen");
 }
 
-cardsInPlay.push("king"); {
+cardsInPlay.push(cardTwo); {
 
 	console.log("User played king");
 }
 if (cardsInPlay.length === 2) {
 	// How many items are in the cardsInPlay array.
-	} else if (cardsInPlay[0] === cardsInPlay [2]) {
+	 if (cardsInPlay[0] === cardsInPlay [1]) {
 		alert("You found a match!");
 	// Checks equality between the two cards in the cardsInPlay array. 
 	} else {
 		alert("Sorry, try again.");
 	// Values are not equal.
 	}
-
+}
 
 var flipCard = function (candid) {
 
