af9def9c4116ddaa3d7dac994642cc8794a7b1fc
Nhung Tran <nhungtran@Nhungs-MacBook-Pro.local>
2019-01-21 06:25:41 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,35 @@
-
+console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "";
+var cardFour = "";
 
-var cardOne = cards[0];
+cardsInPlay.push("queen"); {
 
-cardsInPlay.push("queen");
-
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-
-cardsInPlay.push("king");
-
-console.log("User flipped king");
-
-if (length === 2){
-	// How many items are in the cardsInPlay array.
-} else if (cardsInPlay[0] === cardsInPlay [2]) {
-	console.log("You found a match!")
-	// Checks equality between the two cards in the cardsInPlay array. 
-} else {
-	console.log("Sorry, try again.")
-	// Values are not equal.
+	console.log("User played queen");
 }
+
+cardsInPlay.push("king"); {
+
+	console.log("User played king");
+}
+if (cardsInPlay.length === 2) {
+	// How many items are in the cardsInPlay array.
+	} else if (cardsInPlay[0] === cardsInPlay [2]) {
+		alert("You found a match!");
+	// Checks equality between the two cards in the cardsInPlay array. 
+	} else {
+		alert("Sorry, try again.");
+	// Values are not equal.
+	}
+
+
+var flipCard = function (candid) {
+
+}
+
+
+
+
