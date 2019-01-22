edb74bbd22594ef1bed43f159eed48bf8f385165
Nhung Tran <nhungtrannn@gmail.com>
2019-01-21 18:05:21 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,34 +1,68 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+
+var cards = [
+ {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+ },
+ {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardThree = "";
-var cardFour = "";
-
-cardsInPlay.push(cardOne); {
-
-	console.log("User played queen");
-}
-
-cardsInPlay.push(cardTwo); {
-
-	console.log("User played king");
-}
-if (cardsInPlay.length === 2) {
-	// How many items are in the cardsInPlay array.
-	 if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match!");
-	// Checks equality between the two cards in the cardsInPlay array. 
+var checkForMatch = function() {
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
-	// Values are not equal.
+		console.log("Sorry, try again.");
 	}
+	};
+
+var flipCard = function() {
+    this.getAttribute('data-id'),10;
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    checkForMatch();
+    this.src = cards[cardId].cardImage;
+
+   
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+ };
+
+ var createBoard =function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id, i');
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    createBoard();
 }
 
-var flipCard = function (candid) {
+};
 
-}
+
+
+
+
+
+
 
 
 
